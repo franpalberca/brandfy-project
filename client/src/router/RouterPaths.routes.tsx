52fdbdcb01611ad321b67a719283f1503899d9a6
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2024-01-25 10:52:42 +0100

    feat(client)create basic routerpaths

---
+++ client/src/router/RouterPaths.routes.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HOME, PRIVATE } from "../config/routes/paths"
+import { PublicRoute } from "../components/router/PublicRoute"
+import { PrivateRoute } from "../components/router/PrivateRoute"
+
+const RouterPaths = () => {
+  return (
+    <>
+    <BrowserRouter>
+        <Routes>
+            <Route path={HOME} element={<PublicRoute />}>
+                <Route index element={} />
+            </Route>
+            <Route path={PRIVATE} element={<PrivateRoute />}>
+                <Route index element={} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default RouterPaths

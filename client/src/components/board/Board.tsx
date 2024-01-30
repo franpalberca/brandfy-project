04dd4001a7ebb310ee886bea895d78fbedb33480
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2024-01-30 19:04:53 +0100

    fix(error)fix error in typescript

--- client/src/components/board/Board.tsx
+++ client/src/components/board/Board.tsx
@@ -4,8 +4,7 @@ import {connect} from 'react-redux';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import {Button} from 'react-bootstrap';
-import { createData } from '../../api/data.fetch';
-
+import {createData} from '../../api/data.fetch';
 
 interface LogoInfoInterface {
 	rotation: number;
@@ -15,7 +14,7 @@ interface LogoInfoInterface {
 }
 
 interface TextInfoInterface {
-	textTransform: string;
+	textTransform: 'none' | 'uppercase' | 'lowercase';
 	fontWeight: number;
 	letterSpacing: number;
 	alignment: 'left' | 'center' | 'right';
@@ -47,8 +46,12 @@ const Board = ({companyName, file}: {companyName: string; file: File | null}) =>
 		setShowTextControls(!showTextControls);
 	};
 
-	const handleSliderChange = (key: keyof LogoInfoInterface, value: number) => {
-		setLogoInfo({...logoInfo, [key]: value});
+	const handleSliderChange = (key: keyof LogoInfoInterface, value: number | number[]) => {
+		if (typeof value === 'number') {
+			setLogoInfo({...logoInfo, [key]: value});
+		} else if (Array.isArray(value) && value.length > 0) {
+			setLogoInfo({...logoInfo, [key]: value[0]});
+		}
 	};
 
 	const handleTextChange = (key: keyof TextInfoInterface, value: any) => {
@@ -56,10 +59,10 @@ const Board = ({companyName, file}: {companyName: string; file: File | null}) =>
 	};
 
 	const handleSave = async () => {
-        if (!file) return; 
+		if (!file) return;
 
-        const formData = new FormData();
-        formData.append('companyLogo', file); 
+		const formData = new FormData();
+		formData.append('companyLogo', file);
 		formData.append('rotation', logoInfo.rotation.toString());
 		formData.append('scale', logoInfo.scale.toString());
 		formData.append('verticalPosition', logoInfo.verticalPosition.toString());
@@ -68,15 +71,15 @@ const Board = ({companyName, file}: {companyName: string; file: File | null}) =>
 		formData.append('fontWeight', textInfo.fontWeight.toString());
 		formData.append('letterSpacing', textInfo.letterSpacing.toString());
 		formData.append('alignment', textInfo.alignment);
-		formData.append('companyName', companyName)
-		
-        try {
-            const data = await createData(formData);
-            console.log('Logo guardado:', data);
-        } catch (error) {
-            console.error('Error al guardar el logo:', error);
-        }
-    };
+		formData.append('companyName', companyName);
+
+		try {
+			const data = await createData(formData);
+			console.log('Logo guardado:', data);
+		} catch (error) {
+			console.error('Error al guardar el logo:', error);
+		}
+	};
 
 	return (
 		<BoardStyles>
@@ -99,13 +102,22 @@ const Board = ({companyName, file}: {companyName: string; file: File | null}) =>
 									<div>
 										<label>Case:</label>
 										<ButtonGroup>
-											<Button style={{backgroundColor: 'black', border: 'black', color: 'white'}} variant={textInfo.textTransform === 'none' ? 'primary' : 'outline-primary'} onClick={() => handleTextChange('textTransform', 'none')}>
+											<Button
+												style={{backgroundColor: 'black', border: 'black', color: 'white'}}
+												variant={textInfo.textTransform === 'none' ? 'primary' : 'outline-primary'}
+												onClick={() => handleTextChange('textTransform', 'none')}>
 												Normal
 											</Button>
-											<Button style={{backgroundColor: 'black', border: 'black', color: 'white'}} variant={textInfo.textTransform === 'uppercase' ? 'primary' : 'outline-primary'} onClick={() => handleTextChange('textTransform', 'uppercase')}>
+											<Button
+												style={{backgroundColor: 'black', border: 'black', color: 'white'}}
+												variant={textInfo.textTransform === 'uppercase' ? 'primary' : 'outline-primary'}
+												onClick={() => handleTextChange('textTransform', 'uppercase')}>
 												UPPERCASE
 											</Button>
-											<Button style={{backgroundColor: 'black', border: 'black', color: 'white'}} variant={textInfo.textTransform === 'lowercase' ? 'primary' : 'outline-primary'} onClick={() => handleTextChange('textTransform', 'lowercase')}>
+											<Button
+												style={{backgroundColor: 'black', border: 'black', color: 'white'}}
+												variant={textInfo.textTransform === 'lowercase' ? 'primary' : 'outline-primary'}
+												onClick={() => handleTextChange('textTransform', 'lowercase')}>
 												lowercase
 											</Button>
 										</ButtonGroup>
@@ -121,13 +133,22 @@ const Board = ({companyName, file}: {companyName: string; file: File | null}) =>
 									<div>
 										<label>Alignment:</label>
 										<ButtonGroup>
-											<Button style={{backgroundColor: 'black', border: 'black', color: 'white'}} variant={textInfo.alignment === 'left' ? 'primary' : 'outline-primary'} onClick={() => handleTextChange('alignment', 'left')}>
+											<Button
+												style={{backgroundColor: 'black', border: 'black', color: 'white'}}
+												variant={textInfo.alignment === 'left' ? 'primary' : 'outline-primary'}
+												onClick={() => handleTextChange('alignment', 'left')}>
 												Left
 											</Button>
-											<Button style={{backgroundColor: 'black', border: 'black', color: 'white'}} variant={textInfo.alignment === 'center' ? 'primary' : 'outline-primary'} onClick={() => handleTextChange('alignment', 'center')}>
+											<Button
+												style={{backgroundColor: 'black', border: 'black', color: 'white'}}
+												variant={textInfo.alignment === 'center' ? 'primary' : 'outline-primary'}
+												onClick={() => handleTextChange('alignment', 'center')}>
 												Center
 											</Button>
-											<Button style={{backgroundColor: 'black', border: 'black', color: 'white'}} variant={textInfo.alignment === 'right' ? 'primary' : 'outline-primary'} onClick={() => handleTextChange('alignment', 'right')}>
+											<Button
+												style={{backgroundColor: 'black', border: 'black', color: 'white'}}
+												variant={textInfo.alignment === 'right' ? 'primary' : 'outline-primary'}
+												onClick={() => handleTextChange('alignment', 'right')}>
 												Right
 											</Button>
 										</ButtonGroup>
@@ -157,9 +178,11 @@ const Board = ({companyName, file}: {companyName: string; file: File | null}) =>
 						</ControlsContainer>
 					)}
 				</div>
-					<ButtonContainer>
-					<Button className='button_save' onClick={handleSave}>Save</Button>
-					</ButtonContainer>
+				<ButtonContainer>
+					<Button className="button_save" onClick={handleSave}>
+						Save
+					</Button>
+				</ButtonContainer>
 			</div>
 		</BoardStyles>
 	);
@@ -179,39 +202,38 @@ const BoardStyles = styled.div`
 		grid-template-rows: 1fr;
 		grid-column-gap: 20vh;
 		height: 73vh;
-        position: relative;
-        left: -15vh;
+		position: relative;
+		left: -15vh;
 	}
 	& .board {
 		grid-area: 1 / 1 / 2 / 2;
 		background-image: url(https://i.postimg.cc/N0X3w3kh/pizarra.png);
-        background-repeat: no-repeat;
-        background-size: contain;
+		background-repeat: no-repeat;
+		background-size: contain;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 		position: relative;
-        left: 28vh;
+		left: 28vh;
 		overflow: hidden;
 	}
 	& .sidebar {
 		grid-area: 1 / 2 / 2 / 3;
 		background-color: grey;
-        color: black;
-        width: 90%;
+		color: black;
+		width: 90%;
 	}
 	& .company_image {
 		width: 40px;
 		cursor: pointer;
-        display: flex;
+		display: flex;
 		align-items: center;
 		justify-content: center;
 	}
 	& .company_name {
 		cursor: pointer;
-        
-        display: flex;
+		display: flex;
 		align-items: center;
 		justify-content: center;
 	}
@@ -229,17 +251,17 @@ const ControlsContainer = styled.div`
 `;
 
 const ButtonContainer = styled.div`
-grid-area: 1 / 3 / 2 / 4;
-    display: flex;
-    align-items: center;
-    margin-top: -65vh;
+	grid-area: 1 / 3 / 2 / 4;
+	display: flex;
+	align-items: center;
+	margin-top: -65vh;
 
-	& .button_save{
+	& .button_save {
 		margin-left: -15vh;
 		background-color: black;
 		border: black;
 	}
-	`
+`;
 
 const SliderWithInput = ({value, onChange}: {value: number; onChange: (value: number) => void}) => {
 	return (
@@ -255,8 +277,8 @@ const SliderWithInput = ({value, onChange}: {value: number; onChange: (value: nu
 const SliderContainer = styled.div`
 	display: flex;
 	align-items: center;
-    width: 95%;
-    margin-top: 1vh;
+	width: 95%;
+	margin-top: 1vh;
 `;
 
 const InputBox = styled.div`
@@ -268,7 +290,7 @@ const InputBox = styled.div`
 		width: 50px;
 		margin-right: 5px;
 	}
-	`;
+`;
 
 const ButtonGroup = styled.div`
 	display: flex;
